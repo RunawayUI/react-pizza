856d78de320cbbf97f31fafc04418ad449ec9334
RunawayUI <tosha.grafkof@yandex.ru>
2025-04-06 23:29:25 +0300

    added clicable categories for pizzas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+import pizzas from './assets/pizzas.json'
+
 import "./scss/app.scss";
 
 function App() {
@@ -18,11 +20,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Чизбургер" price="234" />
-            <PizzaBlock title="Маргарита" price="550"/>
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
+            {pizzas.map(pizzaObj => (
+              <PizzaBlock {...pizzaObj} />
+            ))}
           </div>
         </div>
       </div>
