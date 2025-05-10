49b51a49061b5741a2ec3bafb3182d5f25df0f5f
RunawayUI <tosha.grafkof@yandex.ru>
2025-05-10 21:18:46 +0300

    added some styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,11 +15,12 @@ const Home = () => {
             .then(arr => {
                 setItems(arr);
                 setIsLoading(false);
-            })
+        })
+        window.scrollTo(0, 0);
     }, [])
 
     return (
-        <>
+        <div className="container">
             <div className="content__top">
                 <Categories />
                 <Sort />
@@ -30,7 +31,7 @@ const Home = () => {
                     ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
                     : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
             </div>
-        </>
+        </div>
     )
 }
 
